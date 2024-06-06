1e0c22ac28c2326844817cb346daf571e4d556fe
Mohd Ramzan Shareef <107868540+itisRamzan@users.noreply.github.com>
2024-06-06 11:13:07 +0530

    f

--- src/app/api/checkForUpdates/route.ts
+++ src/app/api/checkForUpdates/route.ts
@@ -1,6 +1,6 @@
 import circularTracker from "@/utils/circularTracker";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export async function GET(res: NextApiResponse) {
+export async function GET(res: NextApiRequest) {
     await circularTracker();
 }
