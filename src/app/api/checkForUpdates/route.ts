f9347ef42497eb280dbc7b451bf0f501a5f8d30c
Mohd Ramzan Shareef <107868540+itisRamzan@users.noreply.github.com>
2024-06-06 14:18:55 +0530

    refactor: Add error handling to checkForUpdates route

--- src/app/api/checkForUpdates/route.ts
+++ src/app/api/checkForUpdates/route.ts
@@ -4,6 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(request: NextRequest) {
     try {
         await circularTracker();
+        console.log("I am here");
         return NextResponse.json({ message: 'Done' });
     }
     catch (e) {
