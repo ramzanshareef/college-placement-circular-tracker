0a65a34250a4ba4b01b2f47ee079950963ce1b3a
Mohd Ramzan Shareef <107868540+itisRamzan@users.noreply.github.com>
2024-06-08 14:04:04 +0530

    refactor: Remove commented out code in cron.js

--- src/pages/api/cron.js
+++ src/pages/api/cron.js
@@ -1,7 +1,8 @@
 export default function handler(req, res) {
-    if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
-        return res.status(401).end('Unauthorized');
-    }
+    // if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+    //     return res.status(401).end('Unauthorized');
+    // }
+    console.log("Hello World, I am from Cron");
     res.status(200).end('Hello Cron!');
 }
 
